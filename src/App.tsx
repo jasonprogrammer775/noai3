e40710e40f04447ce699cfcbe9d9d03a56e04374
Jay <74604447+jasonprogrammer775@users.noreply.github.com>
2025-04-29 01:09:40 +0200

    1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-500 to-purple-700 flex items-center justify-center">
+    <h1 className="text-5xl font-bold text-white drop-shadow-lg">
+      Tailwind + Vite + TypeScript is Working! 🚀
+    </h1>
+  </div>
+  )
+}
+
+export default App
